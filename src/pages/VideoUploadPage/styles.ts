9d15b1d603d9d33793e1992863cd2247a6ef7e61
rachel490 <ccori12@naver.com>
2022-05-01 18:59:45 +0900

    :sparkles: 비디오 업로드 용량 제한 기능 추가

--- src/pages/VideoUploadPage/styles.ts
+++ src/pages/VideoUploadPage/styles.ts
@@ -4,58 +4,66 @@ export const Wrap = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-	padding: 20px;
-`;
-export const FileInput = styled.input`
-  display: none;
+  padding: 20px;
 `;
 
-export const Label = styled.label`
-  cursor: pointer;
-  margin-top: 30px;
+export const Form = styled.form`
   width: 100%;
-  border: 2px dashed rgba(22, 24, 35, 0.2);
-  border-radius: 8px;
-  display: flex;
-	flex-direction: column;
-  align-items: center;
-  text-align: center;
-  padding: 30px 0;
+`;
 
-  img {
-    width: 50px;
-    margin-bottom: 10px;
+export const FileInputContainer = styled.div`
+  input {
+    display: none;
   }
 
-  h2 {
-    font-weight: bold;
-    font-size: 16px;
-    margin-bottom: 5px;
-  }
-
-  span {
-    color: ${({ theme }) => theme.color.nav_active_bg};
-  }
-
-  button {
-    font-size: 17px;
-    margin-top: 20px;
-    border-radius: 10px;
-    color: ${({ theme }) => theme.color.text_white};
-    background-color: #fe2c55;
-    width: 180px;
-    height: 35px;
+  label {
+    cursor: pointer;
+    margin: 30px 0;
+    width: 100%;
+    border: 2px dashed rgba(22, 24, 35, 0.2);
+    border-radius: 8px;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
+    text-align: center;
+    padding: 30px 0;
+
+    img {
+      width: 50px;
+      margin-bottom: 10px;
+    }
+
+    h2 {
+      font-weight: bold;
+      font-size: 16px;
+      margin-bottom: 5px;
+    }
+
+    span {
+      color: ${({ theme }) => theme.color.nav_active_bg};
+    }
+
+    button {
+      font-size: 17px;
+      margin-top: 20px;
+      border-radius: 10px;
+      color: ${({ theme }) => theme.color.text_white};
+      background-color: #fe2c55;
+      width: 180px;
+      height: 35px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      &:hover {
+        background-color: #ff1764;
+      }
+    }
 
     &:hover {
-      background-color: #ff1764;
+      border-color: #fe2c55;
+      background-color: rgba(0, 0, 0, 0.03);
     }
   }
-
-  &:hover {
-    border-color: #fe2c55;
-    background-color: rgba(0, 0, 0, 0.03);
-  }
+	
 `;

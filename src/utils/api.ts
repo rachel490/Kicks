0d56f4b4c8ba4022f702be95df4b807e832b4204
Joy Lee <joylee.dev@gmail.com>
2022-05-05 22:19:19 +0900

    ➕ json-server 설치 (Chat 데이터 입력 및 라우트 설정)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,6 @@
-const BASE_URL = 'https://d0281c4b-3ebc-4789-8f00-ce823cf44c52.mock.pstmn.io';
+const BASE_URL = 'http://localhost:8080';
 
 // Chat Page
 export const CHAT_LIST_API = `${BASE_URL}/v1/users/chatting`;
 export const CHAT_ROOM_API = (chat_room_id: number) =>
-  `${BASE_URL}/v1/users/chatting/:${chat_room_id}`;
-
+  `${BASE_URL}/v1/users/chatting/${chat_room_id}`;

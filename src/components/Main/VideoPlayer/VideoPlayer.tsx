4307b5a2c42a774f835f19dc9fe7566d709f713b
rachel490 <ccori12@naver.com>
2022-05-05 17:42:38 +0900

    :bug: 재생 및 정지 버튼 클릭했을 때 재생 및 정지가 적용 안되는 오류 해결

--- src/components/Main/VideoPlayer/VideoPlayer.tsx
+++ src/components/Main/VideoPlayer/VideoPlayer.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './styles';
+import { IoPlayCircle, IoPauseCircle } from 'react-icons/io5';
 
 export const VideoPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState(true);
 
   const controlVideo = (e: React.MouseEvent<HTMLVideoElement>) => {
     const currentVideo = e.currentTarget;
 
-    if (!currentVideo) {return};
-    
-    if (currentVideo.paused) {
+    if (!currentVideo) {
+      return;
+    }
+
+    if (!isPlaying) {
       currentVideo.play();
+      setIsPlaying(true);
     } else {
       currentVideo.pause();
+      setIsPlaying(false);
     }
-  } 
-
+  };
 
   return (
     <S.Wrap>
@@ -24,7 +29,16 @@ export const VideoPlayer = () => {
         muted
         onClick={controlVideo}
         src="https://player.vimeo.com/progressive_redirect/playback/672684920/rendition/540p/540p.mp4?loc=external&oauth2_token_id=57447761&signature=32cb6b2e5e97333e35129877a0a35748af0bec5798a5066753d36999a017a31a"
-      ></video>
+      />
+      {isPlaying ? (
+        <button>
+          <IoPlayCircle className="icon hide" />
+        </button>
+      ) : (
+        <button>
+          <IoPauseCircle className="icon hide" />
+        </button>
+      )}
     </S.Wrap>
   );
 };
--- src/components/Main/VideoPlayer/styles.ts
+++ src/components/Main/VideoPlayer/styles.ts
@@ -1,13 +1,39 @@
 import styled from 'styled-components';
 
 export const Wrap = styled.div`
-	width: 100%;
-	height: 100%;
-	background-color: blue;
+  width: 100%;
+  height: 100%;
+  background-color: blue;
 
-	video {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
+  .icon {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1000;
+    width: 80px;
+    height: 80px;
+    opacity: 0;
+  }
+
+  .hide {
+    animation-name: fadeOut;
+    animation-duration: 3s;
+  }
+
+  @keyframes fadeOut {
+    0% {
+      opacity: 1;
+    }
+
+    100% {
+      opacity: 0;
+    }
+  }
+
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;

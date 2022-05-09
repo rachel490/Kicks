cc50c41e404222ab900ad38fbecf706aa2e0da2a
rachel490 <ccori12@naver.com>
2022-05-09 17:36:11 +0900

    :sparkles: videoItem 재생 api 추가 및 해당 페이지 라우팅 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,7 +5,11 @@ export const CHAT_LIST_API = `${BASE_URL}/v1/users/chatting`;
 export const CHAT_ROOM_API = (chat_room_id: number) =>
   `${BASE_URL}/v1/users/chatting/${chat_room_id}`;
 
-// Profile Page 
-// my -> :userId로 대체 
+// Profile Page
+// my -> :userId로 대체
 export const MY_VIDEOS_API = `${BASE_URL}/v1/users/my/videos`;
 export const MY_LIKES_API = `${BASE_URL}/v1/users/my/likes`;
+
+// Main Page (영상 재생)
+export const VIDEO_ITEM_API = (video_id: number) =>
+  `${BASE_URL}/v1/videos/${video_id}`;

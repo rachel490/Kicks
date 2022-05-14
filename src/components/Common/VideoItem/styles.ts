ae2c349edf444cfe4c18461e4b1a7b47c470070c
rachel490 <ccori12@naver.com>
2022-05-14 16:02:02 +0900

    :recycle: videoItem 삭제, videoList에 코드 합침

--- src/components/Common/VideoItem/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrap = styled.li`
-  video {
-    width: 100%;
-    height: 157px;
-    object-fit: cover;
-  }
-`;
--- src/components/Common/VideoList/styles.ts
+++ src/components/Common/VideoList/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const VideoContent = styled.ul`
   flex: 1;
@@ -7,5 +8,14 @@ export const VideoContent = styled.ul`
     grid-template-columns: repeat(3, 1fr);
     gap: 3px;
     padding: 3px 10px;
+  }
+`;
+
+export const VideoLink = styled(Link)`
+  img {
+    min-width: 115px;
+    width: 100%;
+    height: 157px;
+    object-fit: cover;
   }
 `;

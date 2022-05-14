f7b1180c9dd5e4c567f13f6525f381bfc6690d4e
rachel490 <ccori12@naver.com>
2022-05-14 23:12:37 +0900

    :sparkles: SWR에 fetcherWithToken 추가

--- src/utils/swr.ts
+++ src/utils/swr.ts
@@ -1,3 +1,12 @@
 import axios from 'axios';
 
 export const fetcher = (url: string) => axios.get(url).then(res => res.data);
+
+export const fetcherWithToken = (url: string, token: string) =>
+  axios
+    .get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then(result => result.data);

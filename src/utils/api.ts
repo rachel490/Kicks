7237eb4584d969fb068a9a9c68c63b4959bb8bab
rachel490 <ccori12@naver.com>
2022-05-16 04:36:41 +0900

    :sparkles: 필요한 api들 모두 추가 및 수정

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,38 +2,67 @@ const BASE_URL = 'http://localhost:8080';
 // const BASE_URL = 'https://numble-kicks.shop";
 // const BASE_URL = 'http://13.209.18.253:8080';
 
-// Chat Page
+// CHAT
 export const CHAT_LIST_API = `${BASE_URL}/v1/users/chatting`;
 export const CHAT_ROOM_API = (chat_room_id: number) =>
   `${BASE_URL}/v1/users/chatting/${chat_room_id}`;
 
-// Profile Page
-// my -> :userId로 대체
-export const MY_VIDEOS_API = `${BASE_URL}/v1/users/my/videos`;
-export const MY_LIKES_API = `${BASE_URL}/v1/users/my/likes`;
-export const FOLLOWING_API = `${BASE_URL}/v1/users/following/from`;
-export const FOLLOWER_API = `${BASE_URL}/v1/users/following/to`;
+// PROFILE
+export const PROFILE_API = (user_id: number) =>
+  `${BASE_URL}/v1/users/${user_id}`;
+export const PROFILE_IMAGE_API = `${BASE_URL}/v1/users/image`;
+export const PROFILE_NICKNAME_API = `${BASE_URL}/v1/users/name`;
+export const SEND_EMAIL_CONFIRM_API = `${BASE_URL}/v1/users/email/auth`;
+export const EMAIL_CONFIRM_API = `${BASE_URL}/v1/users/email`;
+export const MY_VIDEOS_API = (user_id: number, last_id?: number) =>
+  `${BASE_URL}/v1/users/${user_id}/videos?last_id=${last_id}`;
+export const MY_LIKES_API = (user_id: number, last_id?: number) =>
+  `${BASE_URL}/v1/users/${user_id}/likes?last_id=${last_id}`;
+export const WITHDRAWAL_API = `${BASE_URL}/v1/users`;
 
-// Main Page (영상 재생)
+// FOLLOW
+export const FOLLOW_API = (to_user_id: number) =>
+  `${BASE_URL}/v1/users/following/${to_user_id}`;
+export const FOLLOWING_API = (from_member: number) =>
+  `${BASE_URL}/v1/users/following/from?from_member=${from_member}`;
+export const FOLLOWER_API = (to_member: number) =>
+  `${BASE_URL}/v1/users/following/to?to_member=${to_member}`;
+
+// VIDEO
+export const VIDEO_LIST_API = `${BASE_URL}/v1/videos`;
 export const VIDEO_ITEM_API = (video_id: number) =>
   `${BASE_URL}/v1/videos/${video_id}`;
 
-export const VIDEO_LIST_API = `${BASE_URL}/v1/videos`;
+// SEARCH
+export const VIDEO_SEARCH_API = (
+  sortBy?: string,
+  keyword?: string,
+  last_id?: number
+) =>
+  `${BASE_URL}/v1/videos/keyword-condition?keyword=${keyword}&sortBy=${sortBy}&last_id=${last_id}`;
+export const BEST_VIDEO_API = (sortBy: string) =>
+  `${BASE_URL}/v1/videos/satatus-condition?sortBy=${sortBy}`;
 
-// Search Page
-export const RECOMMENDED_VIDEO_API = `${BASE_URL}/v1/videos`;
-export const POPULAR_VIDEO_API = `${BASE_URL}/v1/videos`;
-
-// Upload Page
-export const UPLOAD_VIDEO_API = `${BASE_URL}/v1/videos`;
-
-// Login Page
+// LOGIN/LOGOUT
 export const API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
 export const KAKAO_REDIRECT_URI = 'http://localhost:3000/oauth/callback';
 export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 // export const TOKEN_API = `${BASE_URL}/login/oauth2/code/kakao`;
 export const TOKEN_API = `http://13.209.18.253:8080/login/oauth2/code/kakao`;
+export const TOKEN_RENEW_API = `${BASE_URL}/renew`;
+export const LOGOUT_API = `${BASE_URL}/logout`;
 
-// Delete Page
-export const DELETE_VIDEO_API = (video_id: number) =>
-  `${BASE_URL}/v1/videos/${video_id}`;
+// LIKE
+export const LIKE_API = (video_id: number) =>
+  `${BASE_URL}/v1/videos/${video_id}/likes`;
+export const DISLIKE_API = (video_id: number) =>
+  `${BASE_URL}/v1/videos/likes/${video_id}`;
+
+// ADMIN
+export const ADMIN_USER_API = (parameter?: string) =>
+  `${BASE_URL}/v1/admin/users?parameter=${parameter}`;
+export const ADMIN_VIDEO_API = (page: number, size: number) =>
+  `${BASE_URL}/v1/admin/videos?page=${page}&size=${size}`;
+
+// ETC
+export const CATEGORY_LIST_API = `${BASE_URL}/v1/categories`;

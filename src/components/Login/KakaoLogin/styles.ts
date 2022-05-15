fc92d7a87fade385a978a8e59b35858e46c30802
rachel490 <ccori12@naver.com>
2022-05-15 23:53:51 +0900

    :lipstick: LoginPage UI 수정

--- src/components/Login/KakaoLogin/styles.ts
+++ src/components/Login/KakaoLogin/styles.ts
@@ -1,7 +1,14 @@
 import styled from 'styled-components';
 
 export const KakaoButton = styled.a`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+
   img {
     width: 80%;
+    margin-bottom: 150px;
   }
 `;

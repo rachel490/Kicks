cfcb156229fe555efd9cd6e3720364f540f6ede3
Joy Lee <joylee.dev@gmail.com>
2022-05-18 03:47:34 +0900

    ♻ fetcherWithToken 토큰 추가 및 AdminUser 데이터 타입/API 수정

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -4,8 +4,6 @@ const BASE_URL = 'http://localhost:8080';
 
 export const USER_DATA_API = (user_id: number) =>
   `http://13.209.18.253:8080/v1/users/${user_id}`;
-// export const USER_DATA_API = (user_id: number) =>
-//   `${BASE_URL}/v1/users/${user_id}`;
 
 // Chat Page
 export const CHAT_LIST_API = `${BASE_URL}/v1/users/chatting`;
@@ -20,10 +18,6 @@ export const FOLLOWING_API = (user_id: number) =>
   `http://13.209.18.253:8080/v1/users/following/from?from_member=${user_id}`;
 export const FOLLOWER_API = (user_id: number) =>
   `http://13.209.18.253:8080/v1/users/following/to?to_member=${user_id}`;
-// export const FOLLOWING_API = (user_id: number) =>
-//   `${BASE_URL}/v1/users/following/from?from_member=${user_id}`;
-// export const FOLLOWER_API = (user_id: number) =>
-//   `${BASE_URL}/v1/users/following/to?to_member=${user_id}`;
 
 // Video
 // export const VIDEO_ITEM_API = (video_id: number) =>
@@ -32,16 +26,14 @@ export const VIDEO_ITEM_API = (video_id: number) =>
   `http://13.209.18.253:8080/v1/videos/${video_id}`;
 // export const VIDEO_LIST_API = `${BASE_URL}/v1/videos`;
 export const VIDEO_LIST_API = 'http://13.209.18.253:8080/v1/videos';
+export const USERS_VIDEO_API = (user_id: number) =>
+  `http://13.209.18.253:8080/v1/users/${user_id}/videos?last_id=`;
 
 // Search Page
 export const TOP10_VIDEO_API = (sortBy: 'hits' | 'likes') =>
   `http://13.209.18.253:8080/v1/videos/status-condition?sortBy=${sortBy}`;
 export const SEARCHED_VIDEO_API = (keyword: string, sortBy: 'hits' | null) =>
   `http://13.209.18.253:8080/v1/videos/keyword-condition?keyword=${keyword}&sortBy=${sortBy}`;
-// export const TOP10_VIDEO_API = (sortBy: 'hits' | 'likes') =>
-//   `${BASE_URL}/v1/videos/status-condition?sortBy=${sortBy}`;
-// export const SEARCHED_VIDEO_API = (keyword: string, sortBy: 'hits' | null) =>
-//   `${BASE_URL}/v1/videos/keyword-condition?keyword=${keyword}&sortBy=${sortBy}`;
 
 // Login Page
 export const API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
@@ -50,4 +42,10 @@ export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_i
 // export const TOKEN_API = `${BASE_URL}/login/oauth2/code/kakao`;
 export const TOKEN_API = `http://13.209.18.253:8080/login/oauth2/code/kakao`;
 
+// Delete Page
+export const DELETE_VIDEO_API = (video_id: number) =>
+  `${BASE_URL}/v1/videos/${video_id}`;
 
+// Admin Page
+export const ADMIN_USER_API = (keyword: string, page: number, size: number) =>
+  `http://13.209.18.253:8080/v1/users?keyword=${keyword}&page=${page}&size=${size}`;

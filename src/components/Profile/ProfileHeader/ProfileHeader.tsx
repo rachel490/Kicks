5587968af98a249cbcbbc7029c13fe3fe280260b
rachel490 <ccori12@naver.com>
2022-05-18 10:32:27 +0900

    :sparkles: profileHeader에 사용자 정보 조회 api 추가, 데이터 변경

--- src/components/Profile/ProfileHeader/ProfileHeader.tsx
+++ src/components/Profile/ProfileHeader/ProfileHeader.tsx
@@ -1,37 +1,45 @@
-import React from 'react';
-import * as S from './styles';
-import { profileStatData } from '../../../data';
 import { Link } from 'react-router-dom';
+import useSWR from 'swr';
+import * as S from './styles';
+import { USER_DATA_API } from 'utils/api';
+import { fetcherWithToken } from 'utils/swr';
+import { IUserData } from 'data/types';
 
-export const ProfileHeader = () => {
-  const { videos, following, followers } = profileStatData;
-  const userId = 2;
-  const userName = 'rachel_the_it';
+
+export const ProfileHeader = ({ userId }: { userId: number }) => {
+  const { data } = useSWR(USER_DATA_API(userId), fetcherWithToken);
+  const userData = data?.data as IUserData;
 
   return (
     <S.ProfileHeader>
       <img
-        src="https://images.unsplash.com/photo-1651070216681-22fe5a718c06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-        alt="cherryblossom"
+        src={
+          userData.profile_image_url
+            ? userData.profile_image_url
+            : 'https://images.unsplash.com/photo-1651070216681-22fe5a718c06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+        }
+        alt="profile"
         className="profile-img"
       />
       <div className="profile-username">
-        <span>@{userName}</span>
+        <span>@{userData.name}</span>
       </div>
       <S.ProfileStats>
         <li>
-          <strong className="profile-stat-value">{videos}</strong>
+          <strong className="profile-stat-value">{userData.videos}</strong>
           <span className="profile-stat-name">Videos</span>
         </li>
         <li>
-          <Link to={following ? `${userId}/following` : '#'}>
-            <strong className="profile-stat-value">{following}</strong>
+          <Link to={userData.followings ? `${userId}/following` : '#'}>
+            <strong className="profile-stat-value">
+              {userData.followings}
+            </strong>
             <span className="profile-stat-name">Following</span>
           </Link>
         </li>
         <li>
-          <Link to={followers ? `${userId}/follower` : '#'}>
-            <strong className="profile-stat-value">{followers}</strong>
+          <Link to={userData.followers ? `${userId}/follower` : '#'}>
+            <strong className="profile-stat-value">{userData.followers}</strong>
             <span className="profile-stat-name">Followers</span>
           </Link>
         </li>

b063ef82afe8d0f6f612461f956000d293df3237
Joy Lee <joylee.dev@gmail.com>
2022-05-21 01:23:19 +0900

    🚀 API URL 주소 HTTPS로 변경 후 Vercel 배포
    💄 favicon 및 meta data 추가

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://localhost:8080';
-// const BASE_URL = 'https://numble-kicks.shop";
+// const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://www.numble-kicks.shop';
 // const BASE_URL = 'http://13.209.18.253:8080';
 
 // Chat Page
@@ -9,45 +9,43 @@ export const CHAT_ROOM_API = (chat_room_id: number) =>
 
 // Profile Page
 export const USER_DATA_API = (user_id: number) =>
-  `http://13.209.18.253:8080/v1/users/${user_id}`;
+  `${BASE_URL}/v1/users/${user_id}`;
 export const MY_VIDEOS_API = (user_id: number) =>
-  `http://13.209.18.253:8080/v1/users/${user_id}/videos?`;
+  `${BASE_URL}/v1/users/${user_id}/videos?`;
 export const MY_LIKES_API = (user_id: number) =>
-  `http://13.209.18.253:8080/v1/users/${user_id}/likes?`;
+  `${BASE_URL}/v1/users/${user_id}/likes?`;
 export const FOLLOWING_API = (user_id: number) =>
-  `http://13.209.18.253:8080/v1/users/following/from?from_member=${user_id}`;
+  `${BASE_URL}/v1/users/following/from?from_member=${user_id}`;
 export const FOLLOWER_API = (user_id: number) =>
-  `http://13.209.18.253:8080/v1/users/following/to?to_member=${user_id}`;
+  `${BASE_URL}/v1/users/following/to?to_member=${user_id}`;
 
 // Video
 export const VIDEO_ITEM_API = (video_id: number) =>
-  `http://13.209.18.253:8080/v1/videos/${video_id}`;
-export const VIDEO_LIST_API = 'http://13.209.18.253:8080/v1/videos';
+  `${BASE_URL}/v1/videos/${video_id}`;
+export const VIDEO_LIST_API = `${BASE_URL}/v1/videos`;
 export const USERS_VIDEO_API = (user_id: number) =>
-  `http://13.209.18.253:8080/v1/users/${user_id}/videos?last_id=`;
+  `${BASE_URL}/v1/users/${user_id}/videos?last_id=`;
 
 // Search Page
 export const TOP10_VIDEO_API = (sortBy: 'hits' | 'likes') =>
-  `http://13.209.18.253:8080/v1/videos/status-condition?sortBy=${sortBy}`;
+  `${BASE_URL}/v1/videos/status-condition?sortBy=${sortBy}`;
 export const SEARCHED_VIDEO_API = (keyword: string, sortBy: 'hits' | null) =>
-  `http://13.209.18.253:8080/v1/videos/search-condition?keyword=${keyword}&sortBy=${sortBy}&`;
+  `${BASE_URL}/v1/videos/search-condition?keyword=${keyword}&sortBy=${sortBy}&`;
 
 // Login Page
 export const API_KEY = process.env.REACT_APP_KAKAO_API_KEY;
 export const KAKAO_REDIRECT_URI = 'http://localhost:3000/oauth/callback';
 export const KAKAO_LOGIN_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
-// export const TOKEN_API = `${BASE_URL}/login/oauth2/code/kakao`;
-export const TOKEN_API = `http://13.209.18.253:8080/login/oauth2/code/kakao`;
+export const TOKEN_API = `${BASE_URL}/login/oauth2/code/kakao`;
 
 // Liked
-// export const LIKE_API = `${BASE_URL}/v1/videos/:video_id/likes`;
 export const LIKE_API = (video_id: number) =>
-  `http://13.209.18.253:8080/v1/videos/${video_id}/likes`;
+  `${BASE_URL}/v1/videos/${video_id}/likes`;
 export const UNLIKE_API = (video_id: number) =>
-  `http://13.209.18.253:8080/v1/videos/likes/${video_id}`;
+  `${BASE_URL}/v1/videos/likes/${video_id}`;
 
 // Admin Page
 export const ADMIN_USER_API = (keyword: string) =>
-  `http://13.209.18.253:8080/v1/users?keyword=${keyword}`;
+  `${BASE_URL}/v1/users?keyword=${keyword}`;
 export const ADMIN_CONTENT_API = (page: number, size: number, id: string) =>
-  `http://13.209.18.253:8080/v1/admin/videos?page=${page}&size=${size}&user_id=${id}`;
+  `${BASE_URL}/v1/admin/videos?page=${page}&size=${size}&user_id=${id}`;

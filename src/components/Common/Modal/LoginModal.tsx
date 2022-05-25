50304267d7db941606c04ddeac2c81dcabe8d062
rachel490 <ccori12@naver.com>
2022-05-25 17:31:31 +0900

    :recycle: loginModal 컴포넌트 prop 제거 + recoil로 대체

--- src/components/Common/Modal/LoginModal.tsx
+++ src/components/Common/Modal/LoginModal.tsx
@@ -1,21 +1,36 @@
 import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 import * as S from './styles';
+import { LoginModalState } from 'recoil/atom';
 
 export const LoginModal = () => {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useRecoilState(LoginModalState);
   const navigate = useNavigate();
 
   return (
-    <S.Overlay isOpen={true}>
+    <S.Overlay isOpen={isLoginModalOpen}>
       <S.Modal>
         <S.Message>
           로그인 후 이용가능합니다. <br />
           로그인 하시겠습니까?
         </S.Message>
         <S.ButtonContainer>
-          <button className="yes" onClick={() => navigate('/login')}>
+          <button
+            className="yes"
+            onClick={() => {
+              setIsLoginModalOpen(false);
+              navigate('/login');
+            }}
+          >
             예
           </button>
-          <button className="no" onClick={() => navigate('/')}>
+          <button
+            className="no"
+            onClick={() => {
+              setIsLoginModalOpen(false);
+              navigate('/');
+            }}
+          >
             아니오
           </button>
         </S.ButtonContainer>
